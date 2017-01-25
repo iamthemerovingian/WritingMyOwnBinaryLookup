ab2a51560851c71f6922f7d76ba8e9b82b5938d4
Milinda Seneviratne <iamthemerovingian@hotmail.com>
2017-01-25 14:12:23 +0700

    Testing different methods of finding an int in an array which may not contain the said int,

---
+++ WritingMyOwnBinaryLookup/DataFinder.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WritingMyOwnBinaryLookup
+{
+    class DataFinder
+    {
+        private Random random = new Random();
+
+        public DataFinder()
+        {
+
+        }
+
+        public int[] GenerateArray(int size)
+        {
+            int[] theArray = new int[size];
+            theArray = FillArray(theArray);
+
+            return theArray;
+        }
+
+        private int[] FillArray(int[] theArray)
+        {
+            for (int i = 0; i < theArray.Length; i++)
+            {
+                theArray[i] = random.Next(0, 65500);
+            }
+
+            return theArray;
+        }
+
+        public bool FindIntBuiltIn(int value, int[] theArray)
+        {
+            bool successFlag = false;
+
+            var matched = theArray.Where(x => x == value);
+
+            if (matched.Count() > 0)
+            {
+                successFlag = true;
+            }
+
+            return successFlag;
+        }
+
+        public bool FindInt(int value, int[] theArray)
+        {
+            int match = 0;
+            bool SuccessFlag = false;
+
+            foreach (var integerValue in theArray)
+            {
+                if(integerValue == value)
+                {
+                    match = integerValue;
+                    SuccessFlag = true;
+                }
+            }
+
+            return SuccessFlag;
+        }
+
+        public TValue Find<TValue>(TValue value, TValue[] theArray)
+        {
+            TValue foundItem = value;
+
+
+
+            return foundItem;
+        }
+    }
+}
--- WritingMyOwnBinaryLookup/Program.cs
+++ WritingMyOwnBinaryLookup/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,24 @@ namespace WritingMyOwnBinaryLookup
 {
     class Program
     {
+       public static DataFinder finder = new DataFinder();
+        private static Stopwatch sw = new Stopwatch();
+
         static void Main(string[] args)
         {
+            var theArray = finder.GenerateArray(10000000);
+
+            sw.Start();
+            finder.FindInt(255, theArray);
+            sw.Stop();
+            Console.WriteLine($"Elapsed Ticks: {sw.ElapsedTicks}");
+
+            sw.Reset();
+            sw.Start();
+            finder.FindIntBuiltIn(255, theArray);
+            sw.Stop();
+            Console.WriteLine($"Elapsed Ticks: {sw.ElapsedTicks}");
+
         }
     }
 }
